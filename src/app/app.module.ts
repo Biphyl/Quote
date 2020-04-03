67af7e90590f7e66216f2fbcdcf43f5786c2b01f
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-03 14:02:03 +0300

    updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,30 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { DateCountPipe } from './date-count.pipe';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { CountInputComponent } from './count-input/count-input.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateCountPipe } from './date-count.pipe';
+import { CountInputComponent } from './count-input/count-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    DateCountPipe,
+    QuoteDetailComponent,
     QuoteFormComponent,
+    DateCountPipe,
     CountInputComponent,
-    QuoteDetailComponent
+    CountInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
