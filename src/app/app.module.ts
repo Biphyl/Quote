eaa5847d2674149c614a4eef0e194deb0978ac2c
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-03 13:23:30 +0300

    created component count-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { CountInputComponent } from './count-input/count-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     DateCountPipe,
-    QuoteFormComponent
+    QuoteFormComponent,
+    CountInputComponent
   ],
   imports: [
     BrowserModule,
